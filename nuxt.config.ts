b9a36f3b00e297aa7b8f0ed6d78a6bcfdd1e91ab
David Lopez <david.lopez@kauffmaninc.com>
2023-04-26 12:18:47 -0700

    initial commit: nuxt3 with vuetify

---
+++ nuxt.config.ts
@@ -0,0 +1,26 @@
+import vuetify from 'vite-plugin-vuetify'
+import { createResolver } from '@nuxt/kit'
+
+const { resolve } = createResolver(import.meta.url)
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    css: ['vuetify/lib/styles/main.sass', '@mdi/font/css/materialdesignicons.min.css'],
+    build: {
+      transpile: ['vuetify'],
+    },
+    vite: {
+      define: {
+        'process.env.DEBUG': false,
+      },
+    },
+    hooks: {
+        'vite:extendConfig': (config) => {
+          config.plugins.push(
+            vuetify({
+              styles: { configFile: resolve('./settings.scss') },
+            })
+          )
+        }
+    }
+})
