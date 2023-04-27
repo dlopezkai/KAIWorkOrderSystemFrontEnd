ee11e07fb3765e4077e5347eb946d0c14a53662d
David Lopez <david.lopez@kauffmaninc.com>
2023-04-27 13:36:54 -0700

    use pages and layouts

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <header>
+            <nav>
+                <NuxtLink to="/">Home link</NuxtLink>
+                <ul>
+                    <li>
+                        <NuxtLink to="/">Link1</NuxtLink>
+                        <NuxtLink to="/">Link2</NuxtLink>
+                    </li>
+                </ul>
+            </nav>
+            <AuthN></AuthN>
+        </header>
+        <div>
+            <slot />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .router-link-exact-active {
+        color: #12b488;
+    }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="d-flex align-center flex-column">
+    <!-- <AuthN></AuthN> -->
+    <!-- <p>{{ currentUser.name }}</p> -->
+    <v-icon>mdi-home</v-icon>
+    <v-card width="400">
+      <v-card-title>This is a title</v-card-title>
+      <v-card-subtitle>This is a subtitle</v-card-subtitle>
+      <v-card-text> This is content </v-card-text>
+    </v-card>
+    <v-btn class="mt-10">Hello Sass Changes</v-btn>
+  </div>
+</template>
+
+<script setup lang="ts">
+//   import { useAuthStore } from '~/store/auth';
+//   import { storeToRefs } from 'pinia'
+
+//   const authStore = useAuthStore()
+//   const { currentUser } = storeToRefs(authStore)
+</script>
+
+<style scoped>
+
+</style>
