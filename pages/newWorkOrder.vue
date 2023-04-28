ad57d683a80883d5fc180f88737b9d1bfed11e50
David Lopez <david.lopez@kauffmaninc.com>
2023-04-28 08:00:51 -0700

    add redirect logic for when user attempts to go to new WO page when not authenticated

--- pages/newWorkOrder.vue
+++ pages/newWorkOrder.vue
@@ -4,8 +4,16 @@
     </div>
 </template>
 
-<script setup>
-
+<script setup lang="ts">
+import { useAuthStore } from '~/store/auth';
+    const authStore = useAuthStore()
+    const { $msal } = useNuxtApp()
+    onMounted(async () => {
+        await $msal.handleRedirectPromise()
+        if(!authStore.loggedIn) {
+            await signIn()
+        }
+    })
 </script>
 
 <style lang="scss" scoped>
