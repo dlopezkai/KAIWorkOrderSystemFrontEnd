d24308bce0574d085fa2a52d6555514fac196303
David Lopez <david.lopez@kauffmaninc.com>
2023-04-28 14:56:52 -0700

    add vuetify datatables to app

--- pages/index.vue
+++ pages/index.vue
@@ -4,16 +4,8 @@
     <h2>ClickUp Integration App</h2>
     <p>Please login using your Active Directory credentials</p>
   </div>
-  <div v-else class="d-flex align-center flex-column mt-5">
-    <!-- <AuthN></AuthN> -->
-    <!-- <p>{{ currentUser.name }}</p> -->
-    <v-icon>mdi-home</v-icon>
-    <v-card width="400">
-      <v-card-title>This is a title</v-card-title>
-      <v-card-subtitle>This is a subtitle</v-card-subtitle>
-      <v-card-text> This is content </v-card-text>
-    </v-card>
-    <v-btn class="mt-10">Hello Sass Changes</v-btn>
+  <div v-else class="d-flex align-center flex-column">
+    <data-table-comp-server></data-table-comp-server>
   </div>
 </template>
 
