fb29720721e8e22a330a263b2b2d992f69efc6c9
David Lopez <david.lopez@kauffmaninc.com>
2023-05-17 09:53:58 -0700

    add page title setting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,11 @@ const { resolve } = createResolver(import.meta.url)
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+      head: {
+        title: 'Work Order System'
+      }
+    },
     runtimeConfig:{
       API_URL: 'https://kai.huberspace.net',
       public: {
