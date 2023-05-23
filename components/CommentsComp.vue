3692fdf1ecbba553614c99a515cbc5ca54aebc1f
David Lopez <david.lopez@kauffmaninc.com>
2023-05-23 15:26:47 -0700

    revert GET request back to axios, push form data to  commentsData object to reflect realtime updating

--- components/CommentsComp.vue
+++ components/CommentsComp.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
-        <p>{{ comments.data.data }}</p>
+        <!-- <p>{{ comments.data.data }}</p> -->
 
         <v-list lines="two">
             <v-list-item
-                v-for="comment in comments.data.data"
+                v-for="comment in commentsData"
                 :key="comment.id"
-                :title="comment.user.username + ' - ' + comment.date"
+                :title="comment.username + ' - ' + comment.date"
             >
                 <v-list-item-subtitle v-html="comment.comment_text" class="wrap-text"></v-list-item-subtitle>
             </v-list-item>
@@ -17,7 +17,7 @@
         <v-form ref="form" @submit.prevent="sendComment">
             <v-row>
                 <v-col cols="12" sm="10" md="10">
-                    <v-text-field v-model="commentForm[0].comment" class="mr-3 pt-5" placeholder="Comment" outlined clearable></v-text-field>
+                    <v-text-field v-model="commentForm[0].comment_text" class="mr-3 pt-5" placeholder="Comment" outlined clearable></v-text-field>
                 </v-col>
                 <v-col cols="12" sm="2" md="2">
                     <v-btn dark x-large color="blue" class="mt-7" @click="sendComment">Submit</v-btn>
@@ -42,9 +42,10 @@ const { taskid } = toRefs(props);
 // TODO: set assignee to logged-in user
 const commentForm = ref([
   {
-    assignee: 72138402,
-    comment: '',
-    notify_all: false
+    assignee: '',
+    comment_text: '',
+    notify_all: false,
+    username: ''
   },
 ])
 
@@ -52,20 +53,43 @@ const commentForm = ref([
 const commentFormDefault = ref([
   {
     assignee: '',
-    comment: '',
-    notify_all: false
+    comment_text: '',
+    notify_all: false,
+    username: ''
   },
 ])
 
-const { data: comments } = await useAsyncData('comments', () => {
-    return $fetch(`/api/comments?taskid=${taskid.value}`, {
-        method: 'get',
-    })
+// const { data: comments } = await useAsyncData('comments', () => {
+//     return $fetch(`/api/comments?taskid=${taskid.value}`, {
+//         method: 'get',
+//     })
+// })
+
+const commentsData = ref([])
+
+
+onMounted(() => {
+  loadComments()
 })
+
+function loadComments() {
+    axios.get(`${runtimeConfigs.public.API_URL}/task/` + taskid.value + `/comments`)
+    .then((response) => {
+        commentsData.value = response.data.data.map((item) => {
+            return {
+                comment_text: item.comment_text,
+                date: item.date,
+                id: item.id,
+                username: item.user.username,
+            }
+        })
+    })
+    .catch(err => console.log(err))
+}
 
 function sendComment() {
     const data = {
-        comment_text: commentForm.value[0].comment, 
+        comment_text: commentForm.value[0].comment_text, 
         assignee: 72138402, 
         notify_all: false
     }
@@ -77,6 +101,17 @@ function sendComment() {
     })
     .then(function (response){
         console.log(response)
+        
+        // add additional form fields
+        // commentForm.value[0].username = 'John Doe'
+        commentForm.value[0].assignee = 72138402
+        commentForm.value[0].date = response.data.data.date
+
+        // add new form data to commentsData object to be rendered in component
+        commentsData.value.push(commentForm.value[0])
+
+        // reset the form
+        commentForm.value[0] = commentFormDefault
     })
     .catch(function (error) {
         console.log(error)
