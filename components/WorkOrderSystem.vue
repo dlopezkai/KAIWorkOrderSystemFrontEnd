002b254a53d927fa613caa03dd2cbf42fa03f135
David Lopez <david.lopez@kauffmaninc.com>
2023-06-08 12:10:41 -0700

    sort members list in alphabetical order

--- components/WorkOrderSystem.vue
+++ components/WorkOrderSystem.vue
@@ -525,6 +525,11 @@ function loadMembers() {
         value: {color: item.color, email: item.email, id: item.id, initials: item.initials, profile: item.profile, username: item.username}
       }
     })
+
+    // sort members list
+    members.value = members.value.sort((a, b) => 
+      a.title.localeCompare(b.title))
+
     loading.value = false
   })
   .catch(err => console.log(err))
