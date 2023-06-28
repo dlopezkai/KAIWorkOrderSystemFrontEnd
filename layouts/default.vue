c644768e5038e5f7c0e588f1cb3ccff24ca8eb0d
David Lopez <david.lopez@kauffmaninc.com>
2023-06-28 11:16:04 -0700

    cleanup

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,12 @@
 <template>
-      <v-layout>
-        <v-main style="min-height: 300px;">
-            <div>
-              <slot />
-            </div>
-        </v-main>
-      </v-layout>
-
-  </template>
+  <v-layout>
+    <v-main style="min-height: 300px;">
+        <div>
+          <slot />
+        </div>
+    </v-main>
+  </v-layout>
+</template>
 
 <script setup lang="ts">
   import { useAuthStore } from '~/store/auth';
@@ -18,10 +17,10 @@
 </script>
 
 <style scoped>
-    a {
-        color: #000000;
-    }
-    .router-link-exact-active {
-        color: #000000;
-    }
+  a {
+    color: #000000;
+  }
+  .router-link-exact-active {
+    color: #000000;
+  }
 </style>
