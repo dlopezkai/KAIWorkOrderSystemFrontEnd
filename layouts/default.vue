e50867ae04f65b401f2794cafa19c0c64f307155
David Lopez <david.lopez@kauffmaninc.com>
2023-06-28 14:05:27 -0700

    clean up

--- layouts/default.vue
+++ layouts/default.vue
@@ -18,49 +18,49 @@
   <div v-else>
     <v-layout>
       <v-main style="min-height: 300px;">
-          <div>
-            <!-- TODO:find where 'class="bg-deep-purple"' is defined -->
-            <v-navigation-drawer
-              color="#428086"
-              theme="dark"
-              v-model="drawer"
-              clipped 
-              hide-overlay
-            >
-              <div class="text-center">
-                <nuxt-img src="/images/kai-logo.svg" sizes="sm:100vw md:50vw lg:400px" width="200px" class="mt-3 mb-1 pa-1" style="background:white;"/>
-              </div>
-              
-              <v-list color="transparent">
-                <v-list-item prepend-icon="mdi-account-box" @click="toggleShowUsersWorkOrders(true)">
-                  <v-list-item-title title="Show my work orders">My work orders</v-list-item-title>
-                </v-list-item>
+        <div>
+          <!-- TODO:find where 'class="bg-deep-purple"' is defined -->
+          <v-navigation-drawer
+            color="#428086"
+            theme="dark"
+            v-model="drawer"
+            clipped 
+            hide-overlay
+          >
+            <div class="text-center">
+              <nuxt-img src="/images/kai-logo.svg" sizes="sm:100vw md:50vw lg:400px" width="200px" class="mt-3 mb-1 pa-1" style="background:white;"/>
+            </div>
+            
+            <v-list color="transparent">
+              <v-list-item prepend-icon="mdi-account-box" @click="toggleShowUsersWorkOrders(true)">
+                <v-list-item-title title="Show my work orders">My work orders</v-list-item-title>
+              </v-list-item>
 
-                <v-list-item prepend-icon="mdi-account-box-multiple" @click="toggleShowUsersWorkOrders(false)">
-                  <v-list-item-title title="Show all work orders">All work orders</v-list-item-title>
-                </v-list-item>
+              <v-list-item prepend-icon="mdi-account-box-multiple" @click="toggleShowUsersWorkOrders(false)">
+                <v-list-item-title title="Show all work orders">All work orders</v-list-item-title>
+              </v-list-item>
 
-                <v-list-item prepend-icon="mdi-format-list-bulleted" @click="toggleShowCompleted(false)">
-                  <v-list-item-title title="Show non-completed work orders">Not completed</v-list-item-title>
-                </v-list-item>
+              <v-list-item prepend-icon="mdi-format-list-bulleted" @click="toggleShowCompleted(false)">
+                <v-list-item-title title="Show non-completed work orders">Not completed</v-list-item-title>
+              </v-list-item>
 
-                <v-list-item prepend-icon="mdi-playlist-check" @click="toggleShowCompleted(true)">
-                  <v-list-item-title title="Show completed orders">Completed</v-list-item-title>
-                </v-list-item>
+              <v-list-item prepend-icon="mdi-playlist-check" @click="toggleShowCompleted(true)">
+                <v-list-item-title title="Show completed orders">Completed</v-list-item-title>
+              </v-list-item>
 
-                <v-list-item prepend-icon="mdi-form-select" @click="editItem(item)">
-                  <v-list-item-title title="Add a new work order">Add New Work Order</v-list-item-title>
-                </v-list-item>
-              </v-list>
-            </v-navigation-drawer>
+              <v-list-item prepend-icon="mdi-form-select" @click="editItem(item)">
+                <v-list-item-title title="Add a new work order">Add New Work Order</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-navigation-drawer>
 
-            <v-app-bar flat app clipped-left dark color="#92D5D5">
-              <v-app-bar-nav-icon @click.stop="drawer = !drawer" title="Toggle menu display"></v-app-bar-nav-icon>
-              <v-toolbar-title>Work Order System</v-toolbar-title>
-              <AuthN></AuthN>
-            </v-app-bar>
-            <slot />
-          </div>
+          <v-app-bar flat app clipped-left dark color="#92D5D5">
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer" title="Toggle menu display"></v-app-bar-nav-icon>
+            <v-toolbar-title>Work Order System</v-toolbar-title>
+            <AuthN></AuthN>
+          </v-app-bar>
+          <slot />
+        </div>
       </v-main>
     </v-layout>
   </div>
