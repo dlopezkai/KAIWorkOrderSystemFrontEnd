5a75ee87908aa254bf8dca864f544b719ee261d2
David Lopez <david.lopez@kauffmaninc.com>
2023-06-28 11:15:03 -0700

    initial commit and removing commented code

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,33 +1,4 @@
 <template>
-    <!-- <v-card v-if="authStore.loggedIn">
-      <v-layout>
-        <v-app-bar title="ClickUp Integration App">
-            <AuthN></AuthN>
-        </v-app-bar>
-  
-        <v-main style="min-height: 300px;">
-            <div>
-                <slot />
-            </div>
-        </v-main>
-      </v-layout>
-    </v-card>
-
-    <v-card v-else>
-    <v-card>
-      <v-layout>
-        <v-app-bar title="ClickUp Integration App">
-            <AuthN></AuthN>
-        </v-app-bar>
-          
-        <v-main style="min-height: 300px;">
-            <div>
-                <slot />
-            </div>
-        </v-main>
-      </v-layout>
-    </v-card> -->
-
       <v-layout>
         <v-main style="min-height: 300px;">
             <div>
