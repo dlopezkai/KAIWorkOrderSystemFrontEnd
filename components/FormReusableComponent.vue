7589ceabc974664490c00d491def52923d719323
David Lopez <david.lopez@kauffmaninc.com>
2023-07-10 11:21:25 -0700

    add overlay functionality

--- components/FormReusableComponent.vue
+++ components/FormReusableComponent.vue
@@ -71,7 +71,7 @@
                 <v-col class="text-right">
                   <v-btn variant="plain" @click="close" title="Cancel">Cancel</v-btn>
                   <!-- <v-btn :disabled="submitBtnDisabled" class="rounded" color="blue" @click="submit" :title="`${ submitBtnText } work order`">{{ submitBtnText }}</v-btn> -->
-                  <v-btn class="rounded" color="blue" @click="submit" title="Submit">Submit</v-btn>
+                  <v-btn :disabled="submitBtnDisabled" class="rounded" color="blue" @click="submit" title="Submit">Submit</v-btn>
                 </v-col>
               </v-row>
             </v-card-actions>
@@ -94,6 +94,10 @@
 <script setup>
 import axios from 'axios'
 const runtimeConfig = useRuntimeConfig()
+const submitBtnDisabled = ref(false)
+const submitStatusOverlay = ref(false)
+const submitStatus = ref('')
+const submitInfo = ref('')
 
 const props = defineProps({
     recordId: String,
@@ -116,6 +120,24 @@ const formTitle = computed(() => {
   return (props.formAction === 'new') ? 'New ' + props.formTitle + ' Form' : 'Edit ' + props.formTitle + ' Form'
 })
 
+// computed value for work order submit progress messages
+const onSubmitMsg = computed(() => {
+  switch(submitStatus.value) {
+    case 'submitting':
+      return 'Submitting. Please wait...'
+    case 'internal_api_error':
+      return 'There was an issue with the API.'
+    case 'connection_failure':
+      return 'There was an issue submitting your form. Please try again.'
+    case 'success':
+      return props.formTitle + ' submitted successfully.'
+    case 'updated':
+      return props.formTitle + ' updated successfully.'
+    default:
+      return ''
+  }
+})
+
 // form field validation rules
 const rules =
 {
@@ -127,15 +149,22 @@ const rules =
 }
 
 function close() {
-  // if (!props.recordId) {
-  //   if(submitStatus.value === 'success') {
-  //     emit('closeAndReload')
-  //   } else {
-  //     emit('close')
-  //   }
-  // } else {
+  if (props.formAction === 'new') {
+    if(submitStatus.value === 'success') {
+      emit('closeAndReload')
+    } else {
+      emit('close')
+    }
+  } else {
     emit('close')
-  // }
+  }
+}
+
+function resetSubmitStatus() {
+  close()
+  submitStatus.value = ''
+  submitStatusOverlay.value = false
+  // submitBtnDisabled.value = false
 }
 
 // form submit process
@@ -147,9 +176,9 @@ async function submit() {
 }
 
 function save() {
-  // submitInfo.value = ''
-  // submitStatus.value = 'submitting'
-  // submitStatusOverlay.value = true
+  submitInfo.value = ''
+  submitStatus.value = 'submitting'
+  submitStatusOverlay.value = true
   // submitBtnDisabled.value = true
   let method = ''
   let url = ''
@@ -175,19 +204,19 @@ function save() {
     .then(function (response) {
       if (response.status === 200) {
         if (response.data.response_code === 200) {
-          // submitStatus.value = (!props.recordId) ? 'success' : 'updated'
-          // submitInfo.value = (!props.recordId) ? 'Work order URL: ' + window.location.origin + '/workorders?id=' + response.data.data.id : ''
+          submitStatus.value = (props.formAction === 'new') ? 'success' : 'updated'
+          submitInfo.value = (props.formAction === 'new') ? 'Work order URL: ' + window.location.origin + '/workorders?id=' + response.data.data.id : ''
         } else {
-          // submitStatus.value = 'internal_api_error'
-          // submitInfo.value = data
+          submitStatus.value = 'internal_api_error'
+          submitInfo.value = data
           console.log(response)
           return
         }
       }
     })
     .catch(function (error) {
-      // submitStatus.value = 'connection_failure'
-      // submitInfo.value = error
+      submitStatus.value = 'connection_failure'
+      submitInfo.value = error
       console.log(error)
     })
 }
