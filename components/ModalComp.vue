c8dfb5b42c7d731cbb75102b83f5873726e27b33
David Lopez <david.lopez@kauffmaninc.com>
2023-08-01 17:12:24 -0700

    make a reusable template for a modal component

---
+++ components/ModalComp.vue
@@ -0,0 +1,49 @@
+<template>
+    <v-overlay class="align-center justify-center" persistent>
+        <v-container style="height: 400px;">
+            <v-row class="fill-height" align-content="center" justify="center">
+                <v-col class="text-subtitle-1 text-center" cols="12">
+                    <v-card v-if="props.type === 'warning'" style="font-family:'Open Sans;'">
+                        <v-card-title style="background-color:red; color:white">{{ props.cardTitle }}</v-card-title>
+                        <v-spacer></v-spacer>
+                        <v-card-text>{{ props.cardText }}</v-card-text>
+                        <v-card-actions>
+                            <v-row>
+                                <v-col class="text-right">
+                                    <v-btn variant="plain" @click="emit('close')" :title=props.cancelBtnText>{{ props.cancelBtnText }}</v-btn>
+                                    <v-btn class="rounded" color="error" :title=props.confirmBtnText @click="emit('confirm')">{{ props.confirmBtnText }}</v-btn>
+                                </v-col>
+                            </v-row>
+                        </v-card-actions>
+                    </v-card>
+
+                    <v-card v-if="props.type === 'formSubmit'" style="font-family:'Open Sans;'">
+                        <v-card-title>{{ props.cardTitle }}</v-card-title>
+                        <v-card-text v-if="props.cardText">{{ props.cardText }}</v-card-text>
+                        <v-progress-circular v-if="props.submitStatus === 'submitting'" color="#92D5D5" indeterminate size="64" class="mb-4"></v-progress-circular>
+                        <v-btn v-if="props.submitStatus != 'submitting'" color="blue-darken-1" variant="text" class="mb-4" @click="emit('confirm')">
+                            {{ props.confirmBtnText }}
+                        </v-btn>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-overlay>
+</template>
+
+<script setup>
+const props = defineProps({
+    type: String,
+    cardTitle: String,
+    cardText: String,
+    cancelBtnText: String,
+    confirmBtnText: String,
+    submitStatus: String,
+})
+
+const emit = defineEmits(['close', 'confirm'])
+</script>
+
+<style lang="scss" scoped>
+
+</style>
